d2ce8f0cd2dee3f0deba608761c75497d33907f6
Jeferson Rodrigues <jeffrodjs0@gmail.com>
2025-10-27 19:21:31 -0300

    Update scriptindex.js

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -2,20 +2,15 @@ let botao = document.querySelector(".convite")
 let projetobtn = document.querySelector("#projetosconvite")
 
 //enviar o usuario para a pagina de cadastro
-botao.addEventListener("click", ()=>{
-    window.location.href = 'cadastro.html';
-})
+
 botao.addEventListener("click", ()=>{
     window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/cadastro.html';
 })
 
 
 projetobtn.addEventListener("click", ()=>{
-    window.location.href = 'projetos.html';
-
-})
-projetobtn.addEventListener("click", ()=>{
     window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/projetos.html';
 
 })
 x
+
