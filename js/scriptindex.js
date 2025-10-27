8aa49a440051a508d85887d377a09d26084f9658
Jeferson Rodrigues <jeffrodjs0@gmail.com>
2025-10-27 19:19:04 -0300

    - Ajuste nos links do home
    Added external URLs for cadastro and projetos pages.

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -5,7 +5,17 @@ let projetobtn = document.querySelector("#projetosconvite")
 botao.addEventListener("click", ()=>{
     window.location.href = 'cadastro.html';
 })
+botao.addEventListener("click", ()=>{
+    window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/cadastro.html';
+})
+
 
 projetobtn.addEventListener("click", ()=>{
     window.location.href = 'projetos.html';
+
 })
+projetobtn.addEventListener("click", ()=>{
+    window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/projetos.html';
+
+})
+x
