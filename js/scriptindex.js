b08e81ffbdfe10696c0db48ff8742b7ec65c5afd
Jeferson Rodrigues <jeffrodjs0@gmail.com>
2025-11-01 12:54:35 -0300

    Update links to relative paths in scriptindex.js

--- js/scriptindex.js
+++ js/scriptindex.js
@@ -2,15 +2,10 @@ let botao = document.querySelector(".convite")
 let projetobtn = document.querySelector("#projetosconvite")
 
 //enviar o usuario para a pagina de cadastro
-
 botao.addEventListener("click", ()=>{
-    window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/cadastro.html';
+    window.location.href = 'cadastro.html';
 })
-
 
 projetobtn.addEventListener("click", ()=>{
-    window.location.href = 'https://jeferson6191.github.io/Projeto-Front-End-ONG-Jamaican-s-Wish/projetos.html';
-
+    window.location.href = 'projetos.html';
 })
-x
-
